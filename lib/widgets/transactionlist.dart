12fe1fa502249f368f78d5b87627b0e830bda5ac
sushant <aryalsushant564@gmail.com>
2023-01-19 14:51:39 +0545

    Added scrollable functionality with listview

--- lib/widgets/transactionlist.dart
+++ lib/widgets/transactionlist.dart
@@ -1,52 +1,67 @@
 import 'package:flutter/material.dart';
-import "./../models/transaction.dart";
 import 'package:intl/intl.dart';
 
+import '../models/transaction.dart';
+
 class TransactionList extends StatelessWidget {
-  final List<Transaction> _userTransaction;
-  TransactionList(this._userTransaction);
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        child: SingleChildScrollView(
-          child: Column(
-            children: _userTransaction.map((tx) {
-              return Card(
-                  child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    child: Text('\$ ${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple)),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                            style: BorderStyle.solid)),
-                    padding: EdgeInsets.all(10),
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat.yMMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
                   ),
-                ],
-              ));
-            }).toList(),
-          ),
-        ));
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
+    );
   }
 }
